e84cb5abea8d01c48f82068bfa93cd54bd74342d
Lachlan Turner <lachlan.turner@aligent.com.au>
2021-01-15 09:42:20 +1030

    Initial commit of module - give ability to bypass 2FA

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * @author Aligent Consulting Team
+ * @copyright Copyright (c) 2021 Aligent Consulting. (http://www.aligent.com.au)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Aligent_Bypass2FA', __DIR__);
